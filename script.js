10a6407263fcc723e75284dd3afe27c637500554
ekoramadhanu <handiirawanub@gmail.com>
2021-10-13 09:01:55 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+let x, y;
+
+for (x = 1; x <= 8; x++){
+    for (y = 1; y <= 13; y++){
+        if (y == 1 || y == 2 || x == 3 || x == 4 || y == 12 || y == 13) {
+            
+            document.write("H")
+        } else {
+            document.write("X")
+        }
+    }
+    document.write("</br>")
+}
+
+for (x = 1; x <= 8; x++) {
+    for (y = 1; y <= 13; y++) {
+        if ((x == 1 && y == 7)
+            || (x == 2 && y == 5)
+            || (i == 2 && j == 9)
+                        || (i == 3 && j == 4)
+                        || (i == 3 && j == 10)
+                        || (i == 4 && j == 3)
+                        || (i == 4 && j == 5)
+                        || (i == 4 && j == 7)
+                        || (i == 4 && j == 9)
+                        || (i == 4 && j == 11)
+                        || (i == 5 && j == 2)
+                        || (i == 5 && j == 12)
+                        || (i == 6 && j == 1)
+                        || (i == 6 && j == 13)) 
+
+        ) {
+            
+            document.write("H")
+        }
+            else {
+                document.write("Y")
+    }
+    }
+    document.write("</br>")
+}
